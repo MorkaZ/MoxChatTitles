b17e639e225c66f50212066d7dfac2d647ff75d3
BuildTools <unconfigured@null.spigotmc.org>
2018-12-01 10:35:45 +0100

    Migrated titles configuration from config.yml to titles.yml

--- src/main/java/com/morkaz/moxchattitles/configuration/ConfigManager.java
+++ src/main/java/com/morkaz/moxchattitles/configuration/ConfigManager.java
@@ -1,15 +1,32 @@
 package com.morkaz.moxchattitles.configuration;
 
+import com.morkaz.moxchattitles.MoxChatTitles;
+import com.morkaz.moxlibrary.api.ConfigUtils;
 import com.morkaz.moxlibrary.other.configuration.LocaleConfiguration;
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
 
 
 public class ConfigManager extends LocaleConfiguration {
 
-	public ConfigManager(Plugin plugin) {
-		super(plugin, "messages", "pl", "locale");
+	private FileConfiguration titlesConfig;
+	private MoxChatTitles main;
+
+	public ConfigManager(MoxChatTitles main) {
+		super(main, "messages", "pl", "locale");
+		this.main = main;
+		reloadTitlesConfig();
 	}
+
+	public FileConfiguration getTitlesConfig() {
+		return titlesConfig;
+	}
+
+	public void reloadTitlesConfig(){
+		titlesConfig = ConfigUtils.loadFileConfiguration(main, "titles.yml", false);
+	}
+
+
 
 }
